50930aba95e5c7e4f1d91e7922e69beff2ee8446
jeewan10 <jeewanchamlagai10@gmail.com>
2025-06-30 15:26:40 +0545

    push to git

---
+++ corp.js
@@ -0,0 +1,15 @@
+
+$("#r").mouseenter(function(){
+    $("#box").removeClass('d-none');
+ });
+ $("#r").mouseleave(function(){
+    $("#box").addClass("d-none");
+ });
+ $("#b").mouseenter(function(){
+    $("#box1").removeClass('d-none');
+ });
+ $("#b").mouseleave(function(){
+    $("#box1").addClass("d-none");
+ });
+
+
